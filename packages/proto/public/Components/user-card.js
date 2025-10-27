9a5477a8432a1368c97af5d76bf0261d2d2e5253
DrewLoader <loaderandrew1@gmail.com>
2025-10-27 16:13:24 -0700

    adding a user card component

---
+++ packages/proto/public/Components/user-card.js
@@ -0,0 +1,42 @@
+import { LitElement, html } from 'lit';
+
+export class UserCard extends LitElement {
+    static properties = {
+        id: { type: String },
+        region: { type: String },
+        since: { type: String },
+        email: { type: String },
+        sectionId: { type: String }
+    };
+
+    constructor() {
+        super();
+        this.id = '';
+        this.region = '';
+        this.since = '';
+        this.email = '';
+        this.sectionId = 'profile';
+    }
+    
+    createRenderRoot() { return this; }
+
+    render() {
+        return html`
+            <section id = "${this.sectionId}">
+                <h2>
+                    <svg class="icon"><use href="/icons/music.svg#icon-headphone" /></svg>
+                    Profile
+                </h2>
+                <ul class = "list">
+                    <li>ID: ${this.userId}</li>
+                    <li>Region: ${this.region}</li>
+                    <li>Member since: ${this.since}</li>
+                    <li>Email: ${this.email}</li>
+                </ul>
+            </section>
+        `;
+    }
+
+}
+
+customElements.define('user-card', UserCard);
