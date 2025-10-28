d4a1226e65be8d1402f5e1f3ce4783e35c02b883
DrewLoader <loaderandrew1@gmail.com>
2025-10-27 20:29:19 -0700

    testing ts change

---
+++ packages/proto/src/components/artist-details.ts
@@ -0,0 +1,27 @@
+import { LitElement, html } from "lit";
+import { property } from "lit/decorators.js";
+
+export class ArtistDetails extends LitElement {
+  // Keep styling from page.css by using light DOM:
+  protected createRenderRoot() { return this; }
+
+  @property() name = "";
+  @property() genres = "";
+  @property() country = "";
+
+  override render() {
+    return html`
+      <section>
+        <h2>
+          <svg class="icon"><use href="/icons/music.svg#icon-microphone" /></svg>
+          Artist
+        </h2>
+        <dl class="two">
+          <dt>Name</dt><dd>${this.name}</dd>
+          <dt>Genres</dt><dd>${this.genres}</dd>
+          <dt>Country</dt><dd>${this.country}</dd>
+        </dl>
+      </section>
+    `;
+  }
+}
---
+++ packages/proto/src/styles/reset.css.ts
@@ -0,0 +1,7 @@
+import { css } from "lit";
+const styles = css`
+  * { margin: 0; box-sizing: border-box; }
+  img { max-width: 100%; display: block; }
+  ul { list-style: none; padding: 0; }
+`;
+export default { styles };
