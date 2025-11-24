6cebb3066971855292bd06b7366d0a04f093b929
DrewLoader <loaderandrew1@gmail.com>
2025-11-23 22:19:38 -0800

    fix

--- packages/app/vite.config.js
+++ packages/app/vite.config.js
@@ -1,9 +1,23 @@
-export default {
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
   server: {
     proxy: {
-      "/api":  "http://localhost:3000",
+      "/api": "http://localhost:3000",
       "/auth": "http://localhost:3000",
       "/images": "http://localhost:3000"
     }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "login.html")
+      }
+    }
   }
-};
+});
