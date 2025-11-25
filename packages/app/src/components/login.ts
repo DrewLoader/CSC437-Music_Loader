e7e17a6f569a4f213e514c6cc33dd2803e0c9532
DrewLoader <loaderandrew1@gmail.com>
2025-11-24 19:09:08 -0800

    debug

--- packages/app/src/components/login.ts
+++ packages/app/src/components/login.ts
@@ -5,3 +5,7 @@ define({
   "mu-auth": Auth.Provider,
   "login-form": Form.Element
 });
+
+console.log("Defined elements:");
+console.log("mu-auth:", customElements.get("mu-auth"));
+console.log("login-form:", customElements.get("login-form"));
